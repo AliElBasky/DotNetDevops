018e04c8d0853a3d5e4fc3e7d7bb4af3c7425482
Ali El Basky <alibasky12@gmail.com>
2024-11-05 19:17:00 +0200

    first commit

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,27 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Name = "Iphone x",
+                Description = "this iphone is X",
+                ImageFile = "product-1.png",
+                Price = 850.00m,
+                Category = "Smart Phones"
+            },
+            new Product()
+            {
+                Name = "Iphone 11",
+                Description = "this iphone is 11",
+                ImageFile = "product-2.png",
+                Price = 950.00m,
+                Category = "Smart Phones"
+            },
+        };
+    }
+}
